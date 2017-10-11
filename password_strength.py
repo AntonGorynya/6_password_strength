a5332224de70ed95cb69674317f1f15e0577faa4
Anton <antongorynya1@gmail.com>
2017-10-11 15:13:49 -0700

    pep8

--- password_strength.py
+++ password_strength.py
@@ -1,39 +1,36 @@
 import getpass
 
-#password = getpass.getpass('Password: ')
-password = input('Password: ')
-
 
 def get_password_strength(password):
     assessment = 0
-    special_simbols = ['@','#','$','!','?']
-    top_pass = [
-    '123456',
-    '123456789',
-    'qwerty',
-    '12345678',
-    '111111',
-    '1234567890',
-    '1234567',
-    'password',
-    'password1',
-    '123123',
-    '987654321',
-    'qwertyuiop',
-    'mynoob',
-    '123321',
-    '666666',
-    '18atcskd2w',
-    '7777777',
-    '1q2w3e4r',
-    '654321',
-    '555555',
-    '3rjs1la7qe',
-    'google',
-    '1q2w3e4r5t',
-    '123qwe',
-    'zxcvbnm',
-    '1q2w3e']
+    special_simbols = ['@', '#', '$', '!', '?']
+    top_pass = ['123456',
+                '123456789',
+                'qwerty',
+                '12345678',
+                '111111',
+                '1234567890',
+                '1234567',
+                'password',
+                'password1',
+                '123123',
+                '987654321',
+                'qwertyuiop',
+                'mynoob',
+                '123321',
+                '666666',
+                '18atcskd2w',
+                '7777777',
+                '1q2w3e4r',
+                '654321',
+                '555555',
+                '3rjs1la7qe',
+                'google',
+                '1q2w3e4r5t',
+                '123qwe',
+                'zxcvbnm',
+                '1q2w3e'
+                ]
 
     if len(password) < 6:
         assessment += 1
@@ -52,25 +49,24 @@ def get_password_strength(password):
     special_simbols_count = 0
     same_letters_count = 0
     for letter in password:
-        if 'a'<=letter<='z':
+        if 'a' <= letter <= 'z':
             low_letter += 1
-        elif 'A'<=letter<='Z':
+        elif 'A' <= letter <= 'Z':
             upper_letter += 1
-        elif '0'<=letter<='9':
+        elif '0' <= letter <= '9':
             digit_count += 1
-        elif special_simbols.count(letter)>0:
+        elif special_simbols.count(letter) > 0:
             special_simbols_count += 1
-        if password.count(letter)>len(password)/2:
+        if password.count(letter) > len(password)/2:
             same_letters_count += 1
 
-
-    if (upper_letter!=0) and (low_letter!=0):
+    if (upper_letter != 0) and (low_letter != 0):
         assessment = assessment + 1
 
     if (digit_count != 0) and ((upper_letter != 0) and (low_letter != 0)):
         assessment = assessment + 1
 
-    if  special_simbols_count != 0:
+    if special_simbols_count != 0:
         assessment = assessment + 1
     if same_letters_count == 0:
         assessment += 1
@@ -80,4 +76,5 @@ def get_password_strength(password):
     return assessment
 
 if __name__ == '__main__':
-    print('password assesment:',get_password_strength(password))
+    password = input('Password: ')
+    print('password assesment:', get_password_strength(password))
