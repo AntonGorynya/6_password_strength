cedf6e462c937f66878523c3d8075c7d52a50945
Anton <antongorynya1@gmail.com>
2017-10-12 17:57:52 +0300

    Update password_strength.py

--- password_strength.py
+++ password_strength.py
@@ -1,9 +1,22 @@
 import getpass
 
 
-def get_password_strength(password):
+def check_len(password):
     assessment = 0
-    special_simbols = ['@', '#', '$', '!', '?']
+    if len(password) < 6:
+        assessment += 1
+    elif len(password) < 9:
+        assessment += 2
+    elif len(password) < 11:
+        assessment += 3
+    elif len(password) < 13:
+        assessment += 4
+    else:
+        assessment += 5
+    return assessment
+
+def check_top_password(password):
+    assessment = 0
     top_pass = ['123456',
                 '123456789',
                 'qwerty',
@@ -32,17 +45,15 @@ def get_password_strength(password):
                 '1q2w3e'
                 ]
 
-    if len(password) < 6:
-        assessment += 1
-    elif len(password) < 9:
-        assessment += 2
-    elif len(password) < 11:
-        assessment += 3
-    elif len(password) < 13:
-        assessment += 4
-    else:
-        assessment += 5
+    if top_pass.count(password) == 0:
+        assessment = assessment + 1
 
+    return assessment
+
+
+def check_symbols(password):
+    assessment = 0
+    special_simbols = ['@', '#', '$', '!', '?']
     low_letter = 0
     upper_letter = 0
     digit_count = 0
@@ -57,22 +68,25 @@ def get_password_strength(password):
             digit_count += 1
         elif special_simbols.count(letter) > 0:
             special_simbols_count += 1
-        if password.count(letter) > len(password)/2:
+        if password.count(letter) > len(password) / 2:
             same_letters_count += 1
-
     if (upper_letter != 0) and (low_letter != 0):
         assessment = assessment + 1
-
     if (digit_count != 0) and ((upper_letter != 0) and (low_letter != 0)):
         assessment = assessment + 1
-
     if special_simbols_count != 0:
         assessment = assessment + 1
     if same_letters_count == 0:
         assessment += 1
+    return assessment
 
-    if top_pass.count(password) == 0:
-        assessment = assessment + 1
+
+
+def get_password_strength(password):
+    assessment = 0
+    assessment = check_len(password) + assessment
+    assessment = check_symbols(password) + assessment
+    assessment=check_top_password(password) + assessment
     return assessment
 
 if __name__ == '__main__':
