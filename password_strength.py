88c35acba901fa3d92dc1be3d5f0976fe3dc481c
Anton <antongorynya1@gmail.com>
2017-10-20 14:10:31 +0300

    rename function

--- password_strength.py
+++ password_strength.py
@@ -21,44 +21,16 @@ def get_len_assessment(password):
     return assessment
 
 
-def check_no_popular(password):
-    top_pass = ['123456',
-                '123456789',
-                'qwerty',
-                '12345678',
-                '111111',
-                '1234567890',
-                '1234567',
-                'password',
-                'password1',
-                '123123',
-                '987654321',
-                'qwertyuiop',
-                'mynoob',
-                '123321',
-                '666666',
-                '18atcskd2w',
-                '7777777',
-                '1q2w3e4r',
-                '654321',
-                '555555',
-                '3rjs1la7qe',
-                'google',
-                '1q2w3e4r5t',
-                '123qwe',
-                'zxcvbnm',
-                '1q2w3e'
-                ]
-    if password not in top_pass:
-        return True
-    else:
-        return False
+def check_forbiden_pass(password):
+    with open('./forbiden_password.txt') as forbiden_pass_file:
+        forbiden_pass = forbiden_pass_file.readlines()
+    return (password not in forbiden_pass)
 
 
 def symbols_assessment(password):
     frequent_characters = 1
     assessment = 0
-    if set(password).intersection(set(punctuation)) != {}:
+    if set(password).intersection(punctuation) != set():
         assessment += 1
     if (Counter(password).most_common(frequent_characters))[0][1] \
             < len(password) / 2:
@@ -71,19 +43,16 @@ def symbols_assessment(password):
     return assessment
 
 
-def check_data_in_not_password(password, user_data):
+def check_user_data_not_in_password(password, user_data):
     birthday = ''.join(re.findall(r'[0-9]+', user_data['birthday']))
-    if password.find(birthday) >= 0 \
-            and password.find(user_data['company_name']) >= 0:
-        return False
-    else:
-        return True
+    return (password.find(birthday) < 0 and
+            password.find(user_data['company_name']) < 0)
 
 
 def get_password_strength(password, user_data):
     assessment = get_len_assessment(password) + symbols_assessment(password) \
-                 + check_no_popular(password) \
-                 + check_data_in_not_password(password, user_data)
+                 + check_forbiden_pass(password) \
+                 + check_user_data_not_in_password(password, user_data)
     return assessment
 
 if __name__ == '__main__':
